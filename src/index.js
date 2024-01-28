d77966e401cf85b521364cf8ffb8b3b8294c3346
ahgddev <ahgddev@gmail.com>
2024-01-27 20:05:28 -0700

    Game Start section completed.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let gameStarted = false;
+
+//Start the game when the user presses "Let's Start"
+document.getElementById("guess_button").addEventListener("click", function (event) {
+    window.alert("Get ready!")
+    gameStarted = true;
+ });
